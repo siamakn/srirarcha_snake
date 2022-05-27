cb5792747a576a850d547a713b3be3bc04108574
Siamak <siamaknak@yahoo.de>
2022-05-27 22:25:05 +0200

    modified the snake.py, according to Kristians

--- spicy_snake/snake.py
+++ spicy_snake/snake.py
@@ -1,52 +1,82 @@
 import curses
+import time
+
+from spicy_snake.playground import Playground
+from spicy_snake.snake_tail import Snake
+from spicy_snake.screen_helpers import prepare_screen
+
+# ASCII codes of characters on the keyboard
+# KEY_COMMANDS = {97: LEFT, 100: RIGHT, 119: UP, 115: DOWN}
+KEY_COMMANDS = {68: 'left', 67: 'right', 65: 'down', 66: 'up'}
+
+SNAKE_SYMBOL = 'O'
+HEAD_SYMBOL = 'G'
+WALL_SYMBOL = '#'
+FOOD_SYMBOL = '*'
 
 
-LEFT = (-1, 0)
-RIGHT = (1, 0)
-UP = (0, -1)
-DOWN = (0, 1)
-
-KEY_COMMANDS = {97: LEFT, 100: RIGHT, 119: UP, 115: DOWN}
-
-# prepare the screen
-screen = curses.initscr()
-curses.start_color()
-curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-curses.curs_set(0)
-curses.noecho()
-curses.raw()
-screen.keypad(False)
-
-win = curses.newwin(80, 25, 0, 0)
-win.nodelay(True)
-
-
-def game_loop(screen):
-    x, y = 5, 5
-
-    # draw
+def draw(snake, pg, win, screen):
+    # separate functions draw_player and draw_playground
     screen.clear()
-    screen.addch(y, x, "O", curses.color_pair(1))
+
+    # draw the snake
+    for x, y in snake.tail:
+        screen.addch(y, x, SNAKE_SYMBOL, curses.color_pair(1))
+    x, y = snake.head
+    screen.addch(y, x, HEAD_SYMBOL, curses.color_pair(1))
+
+    # draw the playground:
+    for pgx in range(pg.xsize + 1):
+        for pgy in range(pg.ysize + 1):
+            if pg.is_obstacle((pgx, pgy)):
+                screen.addch(pgy, pgx, WALL_SYMBOL, curses.color_pair(2))
+
+    if pg.food:
+        fx, fy = pg.food
+        screen.addch(fy, fx, FOOD_SYMBOL, curses.color_pair(3))
+
     win.refresh()
     screen.refresh()
 
-    while True:
 
-        char = win.getch()
-        direction = KEY_COMMANDS.get(char)
+win, screen = prepare_screen()
+
+
+def move_player(player_position, direction):
+    dx, dy = direction
+    x, y = player_position
+    x += dx
+    y += dy
+    return x, y
+
+
+def game_loop(screen):
+    pg = Playground(30, 14)
+    pg.add_random_food()
+    snake = Snake(5, 5)
+    draw(snake, pg, win, screen)
+
+    delay = 20000
+
+    while not snake.check_collision(pg):
+        # move the player
+        char = win.getch()  # returns the code of a pressed key
+        direction = KEY_COMMANDS.get(char)  # direction is a tuple or None
         if direction:
-            dx, dy = direction
-            x += dx
-            y += dy
+            snake.set_direction(direction)
 
-            # draw
-            screen.clear()
-            screen.addch(y, x, "O", curses.color_pair(1))
-            win.refresh()
-            screen.refresh()
+        delay -= 1
+        if delay == 0:
+            delay = 20000
+            snake.forward()
+            snake.eat(pg)
+            if not pg.food:
+                pg.add_random_food()
+            draw(snake, pg, win, screen)
+
+    time.sleep(2)
 
 
 if __name__ == "__main__":
-
     curses.wrapper(game_loop)
     curses.endwin()
